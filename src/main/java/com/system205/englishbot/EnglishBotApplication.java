b4ae6a89bdedf0a0c95a1c87dfd69329e3e1e1ce
system205 <arsenmutalapov205@gmail.com>
2023-12-06 21:31:26 +0300

    fix - add starter configuration

--- src/main/java/com/system205/englishbot/EnglishBotApplication.java
+++ src/main/java/com/system205/englishbot/EnglishBotApplication.java
@@ -3,9 +3,11 @@ package com.system205.englishbot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Slf4j
+@Import(value = org.telegram.telegrambots.starter.TelegramBotStarterConfiguration.class)
 public class EnglishBotApplication {
 
     public static void main(String[] args) {
